fe9f14b099a630f7ad97669f13222a639ea17151
Kai Devrim <kkgithub@koldkai.com>
2020-04-24 21:42:28 -0700

    Fixed some things

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} connected")
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f"{member.name}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong")
+
+bot.run(TOKEN)
+
