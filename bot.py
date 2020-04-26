e55e566357b858b7c67053d93058afad46f414f5
Soham Bhattacharya <sohamb117@gmail.com>
2020-04-26 14:24:23 -0700

    Finished Warn

--- bot.py
+++ bot.py
@@ -5,7 +5,9 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+
 bot = commands.Bot(command_prefix='>')
+
 
 @bot.event
 async def on_ready():
@@ -33,6 +35,7 @@ async def welcome(ctx, *, arg):
         await ctx.author.add_roles(role)
 
 @bot.command()
+@commands.has_permissions(manage_messages=True)
 async def clear(ctx, amt=5):
     await ctx.send(f"Deleting {amt} messages")
     await ctx.channel.purge(limit=(int(amt)+2))
@@ -46,19 +49,35 @@ async def throwaway(ctx):
         channel = discord.utils.get(ctx.guild.channels, name="throwaway")
         await channel.delete()
 
+@bot.command()
+async def ban(ctx, user: discord.Member):
+    return user
 
 @bot.command()
+@commands.has_permissions(manage_messages=True)
 async def warn(ctx, user: discord.Member, reason, level = 1):
     warn = level
+    admin = discord.utils.get(ctx.guild.roles, name="Admin") 
     for role in user.roles:
-        if "Warning" in role.name:
+        if "Warning #1" in role.name:
             warn += 1
+        if "Warning #2" in role.name:
+            warn += 2
     warn_role = discord.utils.get(ctx.guild.roles, name="Warning #"+str(warn))
-    await user.add_roles(warn_role)
-    embed=discord.Embed(title="Warned", description= f"Warned {user.mention} for {reason}", color=0xff0000)
-    embed.set_author(name="ScribeBot")
-    embed.add_field(name="Warning Number", value=warn, inline=True)
-    await ctx.send(embed=embed)
+
+    if warn <= 2:   
+        await user.add_roles(warn_role)
+        embed=discord.Embed(title="Warned", description= f"Warned {user.mention} for {reason}", color=0xff0000)
+        embed.set_author(name="ScribeBot")
+        embed.add_field(name="Warning Number", value=warn, inline=True)
+        await ctx.send(embed=embed)
+        
+    elif warn >= 3:
+        embed=discord.Embed(title="Banned", description=f"Banned {user.mention} for {reason}. They had 3 strikes.", color=0xff0000)
+        embed.set_author(name="ScribeBot")
+        await ctx.send(embed=embed)
+        await user.ban(reason=reason, delete_message_days=0)
+
 
 
 bot.run(TOKEN)
